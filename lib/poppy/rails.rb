b1cc730bc7c80e65f25263bb213dc2df42c85f8b
Damien Adermann <damienadermann@gmail.com>
2015-10-23 08:55:11 +1100

    Initial commit

---
+++ lib/poppy/rails.rb
@@ -0,0 +1,7 @@
+require "poppy/rails/version"
+
+module Poppy
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/poppy/rails/version.rb
@@ -0,0 +1,5 @@
+module Poppy
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
